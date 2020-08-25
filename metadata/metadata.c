95d4f1ed88da1e01d353df5951ffd10a73a4a2b2
Zoltan Arnold Nagy <nag@zurich.ibm.com>
2020-08-25 15:52:46 +0000

    int -> uint64_t fixes for PhysicalDisk
    Signed-off-by: Zoltán Arnold Nagy <nag@zurich.ibm.com>

--- metadata/metadata.c
+++ metadata/metadata.c
@@ -1,6 +1,6 @@
 #include <metadata.h>
 
-PhysicalDisk create_physical_disk(int sector_count, int sector_size)
+PhysicalDisk create_physical_disk(uint64_t sector_count, uint64_t sector_size)
 {
         PhysicalDisk physical_disk = PHYSICAL_DISK__INIT;
         physical_disk.key = 111444890ULL;
